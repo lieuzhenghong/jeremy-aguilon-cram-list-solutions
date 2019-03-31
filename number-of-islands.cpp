bb1eea468401631055a3aa2d8bd197faf754a43a
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2019-04-01 03:35:33 +0800

    Clean up Number of Islands code somewhat

--- number-of-islands.cpp
+++ number-of-islands.cpp
@@ -4,7 +4,7 @@ public:
     set<int> islands;
     
     // while leader doesn't point to itself,
-    // traverse until we point to a leader
+    // traverse (by leader) until we reach a node that points to itself
     int get_root(int i)
     {
         if (graph[i].first == i)
@@ -19,10 +19,8 @@ public:
     // get the roots and point b to a
     void join(int i, int j)
     {
-        //cout << "Joining " << i << " and " << j << endl;
         int a = get_root(i);
         int b = get_root(j);
-        
         graph[b].first = a;
     }
     
@@ -44,6 +42,7 @@ public:
         
         int width = grid[0].size();
         
+        // First flatten the 2D array into a map
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
@@ -53,75 +52,51 @@ public:
             }
         }
 
+        // Iterate over all cells and join neighbours together
+        // (point B's leader to A's leader) if they are both land cells.
         
-        if (height == 1)
-        {          
-            // iterate through the grid, and join them if they are both land
-            
-            for (int i = 0; i < width-1; i++)
-            {
-                // compare the two of them and join them together if they are both land
-                
-                if (graph[i].second == 1 && graph[i+1].second == 1)
-                {
-                    join(i, i+1);
-                    flatten(i, i+1);
-                }
-            }
-        }
-        
-        else
+        for (int i = 0; i < height; i++)
         {
-            for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
             {
-                for (int j = 0; j < width; j++)
-                {
-                    // compare all four neighbours and join all land neighbours
-                    int idx = j + (i*width);
-                    int top = j + (i-1)*width;
-                    int bot = j + (i+1)*width;
-                    int left = j + (i*width) - 1;
-                    int right = j + (i*width) + 1;
+                // compare all four neighbours and join all land neighbours
+                // i realise don't compare all four neighbours; only compare bottom
+                // and right will cover all already. Otherwise we might actually
+                // break the SCC
+                
+                int idx = j + (i*width);
+                int bot = j + (i+1)*width;
+                int right = j + (i*width) + 1;
                     
-                    if (grid[i][j] == 49)
+                if (grid[i][j] == 49)
+                {
+                    if (j < width -1 && grid[i][j+1] == 49)
                     {
-                        if (j < width -1)
-                        {
-                            if (grid[i][j+1] == 49)
-                            {
-                                //cout << "Joining right" << endl;
-                                join(idx, right);
-                                flatten(idx, right);
-                            }
-                        }
-                        if (i < height - 1 && grid[i+1][j] == 49)
-                        {
-                            //cout << "Joining bottom" << endl;
-                            join(idx, bot);
-                            flatten(idx, bot);
-                        }
+                        join(idx, right);
+                        flatten(idx, right);
+                    }
+                    if (i < height - 1 && grid[i+1][j] == 49)
+                    {
+                        join(idx, bot);
+                        flatten(idx, bot);
                     }
                 }
             }
         }
         
-        
         // count the number of islands
-            
+        // First flatten all the nodes of the graph    
         for (auto &it : graph) 
         {
-            int key = it.first, value1 = it.second.first, value2 = it.second.second;
-            //cout << key << " " << value1 << " " << value2 << endl;
+            int key = it.first, value1 = it.second.first;
             flatten(value1, key);
         }
+        // Then, count the number of unique leaders
         for (auto &it : graph) 
         {
-            int key = it.first, value1 = it.second.first, value2 = it.second.second;
-            //cout << key << " " << value1 << " " << value2 << endl;
-        
+            int value1 = it.second.first, value2 = it.second.second;        
             if (value2) islands.insert(value1);
         }
         return islands.size(); 
-        
     }
 };
